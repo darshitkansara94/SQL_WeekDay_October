935cd59405261bedf93a9395a42cd699b5f499f4
darshitkansara94 <48837243+darshitkansara94@users.noreply.github.com>
2024-10-21 09:03:18 +0530

    Date 21 Oct 2024
    Database

---
+++ 21-Oct-2024/Database.sql
@@ -0,0 +1,28 @@
+Database : (DB)
+	Database is where we can store our data.
+	It is collection of Tables, Store procedures, Views, Functions, Triggers etc..
+	We can create multiple database into a single server.
+	Every DB name must be unique.
+	Space is not allowed in DB name.
+
+	-- Syntax 
+		Create database database_name
+
+		-- To delete DB
+			Drop database database_name
+
+	-- Example
+		-- Create a new DB
+		Create database ECommerce_DB
+
+		-- Delete database Database_Name
+		Drop database Database_name
+		
+		-- How we can switch between Two DB:
+			We have to use "Use" statement to switch between two diff DB.
+			-- Syntax
+			Use database_name
+
+			-- Example
+				Use CrystalAndStone
+
